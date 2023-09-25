c43e0f5e8d2647ccfc7d6ade05309154af887e5e
Adam Winnicki <adam.baku@gmail.com>
2023-09-25 11:43:36 +0200

    feat(shared): add db context base

---
+++ src/Shared/Database/DbContextBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HRP.Shared;
+
+public abstract class DbContextBase : DbContext
+{
+    private const string CONNECTION_STRING_NAME = "Hrp";
+    protected abstract string Schema { get; }
+
+    protected readonly IConfiguration configuration;
+
+    protected DbContextBase(IConfiguration configuration)
+        : this(new DbContextOptions<DbContextBase>(), configuration) {}
+
+    protected DbContextBase(DbContextOptions options, IConfiguration configuration)
+        : base(options)
+    {
+        this.configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString(CONNECTION_STRING_NAME));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schema);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
