042c56b4be3c49e4b1feed5166affacc0bcd13e8
Adam Winnicki <adam.baku@gmail.com>
2023-09-25 11:40:55 +0200

    feat(shared): add module interface and application part extension

---
+++ src/Shared/Module/IModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HRP.Shared;
+
+public interface IModule
+{
+    void Configure(WebApplication application);
+    void Register(WebApplicationBuilder builder);
+}
---
+++ src/Shared/Module/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRP.Shared;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationPart(this IServiceCollection services, string moduleName)
+    {
+        return services.AddApplicationPart(AppDomain.CurrentDomain.GetAssemblies().First(x => x.FullName!.StartsWith(moduleName + ",")));
+    }
+
+    public static IServiceCollection AddApplicationPart(this IServiceCollection services, Assembly assembly)
+    {
+        var partManager = GetApplicationPartManager(services);
+        
+        partManager.ApplicationParts.Add(new AssemblyPart(assembly));
+
+        return services;
+    }
+ 
+    private static ApplicationPartManager GetApplicationPartManager(IServiceCollection services)
+    {
+        return (ApplicationPartManager)services
+            .Last(descriptor => descriptor.ServiceType == typeof(ApplicationPartManager))
+            .ImplementationInstance;
+    }
+}
