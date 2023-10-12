2204fdcf9d41e79cdeb69eaee54eb30c69bb4bf5
Adam Winnicki <adam.baku@gmail.com>
2023-10-12 16:32:56 +0200

    feat(payroll): command for adding obligation

---
+++ src/Modules/Payroll/Application/Command/AddObligationCommand.cs
@@ -0,0 +1,12 @@
+﻿using HRP.Module.Payroll.Shared;
+using HRP.Shared.Command;
+
+namespace HRP.Module.Payroll.Application.Command;
+
+public class AddObligationCommand(int employeeId, string bankAccount, decimal amount, Currency currency) : ICommand
+{
+    public int EmployeeId => employeeId;
+    public string BankAccount => bankAccount;
+    public decimal Amount => amount;
+    public Currency Currency => currency;
+}
---
+++ src/Modules/Payroll/Application/Command/AddObligationCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using HRP.Module.Payroll.Domain.Entity;
+using HRP.Module.Payroll.Domain.Exception;
+using HRP.Module.Payroll.Domain.Repository;
+using HRP.Shared.Command;
+
+namespace HRP.Module.Payroll.Application.Command;
+
+public class AddObligationCommandHandler(IObligationRepository obligationRepository, IEmployeeRepository employeeRepository) : ICommandHandler<AddObligationCommand>
+{
+    /// <exception cref="EmployeeNotExistsException" />
+    /// <exception cref="IncorrectAmountException" />
+    public async Task HandleAsync(AddObligationCommand command)
+    {
+        if (!await employeeRepository.ExistsAsync(command.EmployeeId))
+        {
+            throw EmployeeNotExistsException.ForId(command.EmployeeId);
+        }
+
+        var obligation = new Obligation(
+                new(command.EmployeeId, command.BankAccount),
+                new(command.Amount, command.Currency)
+            );
+
+        await obligationRepository.PersistAsync(obligation);
+    }
+}
