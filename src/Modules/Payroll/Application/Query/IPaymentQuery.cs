6ad67c70c590ab4e61da2aa9dcb1efa6ec8a2c94
Adam Winnicki <adam.baku@gmail.com>
2023-10-16 11:32:59 +0200

    feat(payroll): add payment query

---
+++ src/Modules/Payroll/Application/Query/IPaymentQuery.cs
@@ -0,0 +1,8 @@
+﻿using HRP.Module.Payroll.Application.Query.ViewModel;
+
+namespace HRP.Module.Payroll.Application.Query;
+
+public interface IPaymentQuery
+{
+    Task<IEnumerable<PaymentView>> GetPaymentsAsync(PaymentFilter filter);
+}
---
+++ src/Modules/Payroll/Application/Query/ViewModel/PaymentView.cs
@@ -0,0 +1,11 @@
+﻿using HRP.Module.Payroll.Shared;
+
+namespace HRP.Module.Payroll.Application.Query.ViewModel;
+
+public record class PaymentView
+{
+    public int ObligationId { get; init; }
+    public string EmployeeFullName { get; init; }
+    public string AmountWithCurrency { get; init; }
+    public DateTime PaidAt { get; init; }
+}
