69cec41940592c269fb3082dfb5f4d0bdef38908
Adam Winnicki <adam.baku@gmail.com>
2023-10-18 19:48:46 +0200

    tests(payroll): SalaryService tests

---
+++ tests/Modules/Payroll/Domain/Service/SalaryServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using HRP.Module.Payroll.Domain.Service;
+using HRP.Module.Payroll.Shared;
+using HRP.Shared.Model;
+
+namespace HRP.Tests.Modules.Payroll.Domain;
+
+public class SalaryServiceTests
+{
+    [Fact]
+    public void ShouldCalculateFullTimeB2BSalaryForHR()
+    {
+        //given
+        var department = Department.HR;
+        var employmentOption = EmploymentOption.FullTime;
+        var employmentForm = EmploymentForm.B2B;
+
+        //when
+        var service = new SalaryService();
+        var result = service.CalculateSalaryFor(department, employmentOption, employmentForm);
+
+        //then
+        Assert.Equal(1518.435m, result.Value);
+        Assert.Equal(Currency.PLN, result.Currency);
+    }
+
+    [Fact]
+    public void ShouldCalculateHalfTimeB2BSalaryForHR()
+    {
+        //given
+        var department = Department.HR;
+        var employmentOption = EmploymentOption.HalfTime;
+        var employmentForm = EmploymentForm.B2B;
+
+        //when
+        var service = new SalaryService();
+        var result = service.CalculateSalaryFor(department, employmentOption, employmentForm);
+
+        //then
+        Assert.Equal(759.2175m, result.Value);
+        Assert.Equal(Currency.PLN, result.Currency);
+    }
+}
